a1b35d85edf407ba64a87de157e6b34f1c3d9e01
mbendev <mathieu.ben@univ-rennes2.fr>
2024-11-14 15:13:49 +0100

    Add files via upload
    Première version du fichier

---
+++ employe.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Définition des classes
+class Employe:
+    def __init__(self, identifiant, nom):
+        self.id = identifiant
+        self.nom = nom
+        self.type = "Employé"
+    
+    def __repr__(self):
+        chaine = "{} {} : {}".format(self.type, self.id, self.nom)
+        return chaine
+    
+class Responsable(Employe):
+    def __init__(self, identifiant, nom, rang):  
+        self.id = identifiant
+        self.nom = nom
+        self.type = "Responsable"
+        self.number = rang
+  
+    def __repr__(self):
+        chaine = "{} {} : {} (number {})".format(self.type, self.id, self.nom, self.number)
+        return chaine
+    
+    def engueuler(self, employe):
+        print("Alors, tu bosses un peu {} ou quoi ?!!".format(employe.nom))
+
+# Programme principal
+        
+monEmploye = Employe("E001", "DURAND")
+print(monEmploye)
+
+monResponsable = Responsable("R001", "MARTIN", 2)
+print(monResponsable)
